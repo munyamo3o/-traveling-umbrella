09274dbd41a2881e440ee99d730870fb8f4d224f
munyamo3o <munyamo3o@gmail.com>
2026-07-07 12:27:07 +0900

    Add files via upload

--- javascript/script.js
+++ javascript/script.js
@@ -95,7 +95,7 @@ setInterval(() => {
     if (poolIndex >= textPool.length) {
         poolIndex = 0; // 最後までいったら最初に戻る
     }
-}, 3000); // 3秒
+}, 2000); // 2秒
 
 // ボタンクリックとEnterキーのイベント登録
 button.addEventListener('click', sendText);
